71922ff2e27602ef83261985201a03a73a47dc1a
Geepesh Agrawal <92394142+Geepesh@users.noreply.github.com>
2022-12-25 20:26:14 +0530

    server code changed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 9000 || process.env.PORT;
 app.set('view engine','ejs')
-
+app.engine('ejs', require('ejs').__express);
 app.get('/',(req,res)=>{
     res.render('index',{
         data : 'data (server.js)'
